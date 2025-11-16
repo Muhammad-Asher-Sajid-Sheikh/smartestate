6940a4abc8b64e051dc1e1554a6b2471112faf4d
Asher Sajid <asher.sajid@cloudjunction.cloud>
2025-11-16 12:41:36 +0500

    undo for Property only

--- frontend/src/pages/PropertyPage.jsx
+++ frontend/src/pages/PropertyPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import PropertyForm from "../components/PropertyForm";
 import PropertyTable from "../components/PropertyTable";
-import UndoButton from "../components/UndoButton";
+//import UndoButton from "../components/UndoButton";
 
 export default function PropertyPage() {
   return (
     <div className="container">
       <PropertyForm />
       <PropertyTable />
-      <UndoButton />
     </div>
   );
 }
--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import TransactionForm from "../components/TransactionForm";
 import PendingTransactions from "../components/PendingTransactions";
-import UndoButton from "../components/UndoButton";
+//import UndoButton from "../components/UndoButton";
 
 export default function TransactionPage() {
   return (
     <div className="container">
       <TransactionForm />
       <PendingTransactions />
-      <UndoButton />
     </div>
   );
 }
