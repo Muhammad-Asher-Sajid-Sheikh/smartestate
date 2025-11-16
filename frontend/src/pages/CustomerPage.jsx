8572f67cc2b6d3f240304d9a609aacad27a91cc2
Asher Sajid <asher.sajid@cloudjunction.cloud>
2025-11-16 12:42:31 +0500

    commit update

--- frontend/src/pages/CustomerPage.jsx
+++ frontend/src/pages/CustomerPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import CustomerForm from "../components/CustomerForm";
 import CustomerTable from "../components/CustomerTable";
-import UndoButton from "../components/UndoButton";
+//import UndoButton from "../components/UndoButton";
 
 export default function CustomerPage() {
   return (
     <div className="container">
       <CustomerForm />
       <CustomerTable />
-      <UndoButton />
     </div>
   );
 }
